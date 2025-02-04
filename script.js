e40b4261b5de4fe1d2119f222544a6558f82fedc
Ruoowen <89079349+Ruoowen@users.noreply.github.com>
2025-02-04 19:52:39 +0100

    MainPage

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+        alert("你点击了按钮！");
+    });
+});
