aaa360666a4da4ce7cdde13501cf18c3b96c9202
Ruoowen <89079349+Ruoowen@users.noreply.github.com>
2025-02-12 11:59:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("myButton");
     button.addEventListener("click", function() {
-        alert("你点击了按钮！");
+        alert("You checked the bottom！");
     });
 });
